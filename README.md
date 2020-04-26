2e444d9110feff847754a47b2891b5d76d4034b5
Miguel Soares de Oliveira <miguelosoares1@hotmail.com>
2020-04-26 18:29:36 -0300

    Challenge finished

---
+++ README.md
@@ -0,0 +1,7 @@
+## [Fundamentos do Node.js](https://github.com/Rocketseat/bootcamp-gostack-desafios/tree/master/desafio-fundamentos-nodejs)
+
+### Cheatsheet
+
+* Install dependencies: `yarn` | `npm i`
+* Run your tests: `yarn test` | `npm test`
+* Run development server (with nodemon): `yarn dev:server` | `npm run dev:server`
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,16 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    const transactions = this.transactionsRepository.getBalance();
+
+    if (type === 'outcome' && value > transactions.total) {
+      throw Error(
+        `You don't have a valid balance to execute an outcome type transaction`,
+      );
+    }
+
+    return this.transactionsRepository.create({ title, value, type });
   }
 }
 
